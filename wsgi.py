13ffa7fcbb3ee4b763c861f1c560a94a5a352148
mknuts <23149051+mknuts@users.noreply.github.com>
2018-03-06 16:15:49 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,6 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def index():
-    return "Flask App!"
- 
-#@app.route("/hello/<string:name>")
-@application.route("/hello/<string:name>/")
 def hello(name):
 #    return name
     quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
